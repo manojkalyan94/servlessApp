0775dcfe66af8b63caf910868fbf4149ea9006d1
Manoj Kalyan <Manoj.Kalyan@thomsonretures.com>
2020-02-20 11:54:55 +0530

    changes in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import { PostEmployeeService } from './Service/post-employee.service';
 })
 export class AppComponent {
   title = 'Applicationform';
-  test="manoj kalyan"
+  
 
-  employeeModel=  new Employee("rob","sharma","abc@gmail.com",1234567890,"s3data")
+  employeeModel=  new Employee("Manoj","Kalyan","abc@gmail.com",1234567890,"s3data")
 
   constructor(private postservice:PostEmployeeService){}
   onSubmit(){
